b321d021b3f56c50563974b5efd1654ad5f423a5
Dave <davy.castanet@gmail.com>
2019-07-05 18:42:37 -0400

    almost done
    works but have to fix the if (e.getIsPet() == true) by changing the visibility of the attributes

--- src/testingJava/Invoice.java
+++ src/testingJava/Invoice.java
@@ -1,35 +1,72 @@
 package testingJava;
 
 import java.util.ArrayList;
-//import java.util.Arrays;
+import java.util.Scanner;
 
 public class Invoice {
-	private double discount = 0.2 ;
-	
+
 	ArrayList<Item> item = new ArrayList<Item>();
-	
-	
+
 	public void add(Item anItem) {
 		item.add(anItem);
 	}
-	
-	
+
 	public double getDiscount() {
-		int res = 0;
-		for(int i = 0; i < item.size();i++) {
-			Item monItem = item.get(i);
-			monItem.getIsPet();
-			res += monItem.getQuantity();
+		boolean isPet = false;
+		double petPrice = 0;
+		double otherPrice = 0;
+		int totalQuantity = 0;
+
+		// enhanced for loop
+		for (Item e : item) {
+			if (e.getIsPet() == true) { //c'est la le pb
+				isPet = true;
+				petPrice = e.getPrice() * e.getQuantity();
+			} else {
+				otherPrice = e.getPrice() * e.getQuantity();
+				totalQuantity += e.getQuantity();
+			}
+		} // for
+
+		if (isPet && totalQuantity >= 5) {
+			return petPrice + otherPrice * 0.8;
+		} else {
+			return petPrice + otherPrice;
 		}
-		
-		
-		
-		return discount;
+
+	}// getDiscount()
+
+	@Override
+	public String toString() {
+		return "item " + item;
 	}
-	
-	 @Override
-	    public String toString() { 
-	        return "item " + item; 
-	    } 	
-	
+
+	public static void main(String[] args) {
+		Invoice invoice = new Invoice();
+		Scanner sc = new Scanner(System.in);
+		boolean done = false;
+
+		while (!done) {
+			double prix;
+			int quantite;
+			boolean isPet = false;
+
+			System.out.println("Quel est le prix de l'item que vous voulez ajouter ?");
+			prix = sc.nextDouble();
+			System.out.println("Quel est la quantite de l'item que vous voulez ajouter ?");
+			quantite = sc.nextInt();
+			System.out.println("Est ce un animal ? 0-oui 1-non ");
+			isPet = sc.nextInt() == 1;
+			System.out.println("Avez-vous d'autre articles ? 0-oui 1-non");
+			done = sc.nextInt() == 1;
+
+			// ajoute l'item
+			invoice.add(new Item(prix, isPet, quantite));
+
+		} // while
+
+		System.out.println("Le total de votre facture est de  " + invoice.getDiscount());
+
+	}// main
+
 }
--- src/testingJava/MainArrayList.java
+++ src/testingJava/MainArrayList.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class MainArrayList {
 
 	public static void main(String[] args) {
-		boolean exit = false; 
-		
-		Item doggo = new Item(100,true,1); 
 		Invoice invoice = new Invoice();
+		Scanner sc = new Scanner(System.in);
+		boolean done = false; 
+
+		
+		/*Item doggo = new Item(100,true,1); 
 		invoice.add(doggo);
 		System.out.println(invoice);
+		*/
 		
-		while(!exit) {
-			Scanner sc = new Scanner(System.in);
+		while(!done) {
+			
+			
+			
 			System.out.println("Entrez un item ");
 			
 			
