825e90db78267f0385f1fb8a4ddc0125583ae0f5
Dave <davy.castanet@gmail.com>
2019-07-08 16:10:13 -0400

    Creation of 2 new classes
    Created Customer and Store to display the name of the customer who bought the most items

---
+++ src/arrayList2/Customer.java
@@ -0,0 +1,29 @@
+package arrayList2;
+
+/** 
+ * On va creer une classe Customer avec un constructeur contenant un nom et une quantite.
+ * On aura egalement des accesseurs pour chaque champs. 
+ * 
+ * @author Davy Castanet 
+ * */
+
+
+public class Customer {
+	String name;
+	double amount;
+	
+	public Customer(String name, double amount) {
+		this.name = name;
+		this.amount = amount; 
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public double getAmount() {
+		return amount; 
+	}
+	
+	
+}
---
+++ src/arrayList2/Store.java
@@ -0,0 +1,55 @@
+package arrayList2;
+
+import java.util.ArrayList;
+
+public class Store {
+	// Store contient un ArrayList comme champs
+	ArrayList<Customer> list = new ArrayList<Customer>();
+
+	/**
+	 * On va ajouter des clients a la list du ArrayList
+	 * 
+	 * @param customerName
+	 * @param amount
+	 */
+	public void addSale(String customerName, double amount) {
+		list.add(new Customer(customerName, amount));
+	}
+
+	/**
+	 * On va retourner le nom du customer avec le plus gros amount
+	 * 
+	 * @return nameMax - le nom du customer avec le plus gros amount
+	 */
+	public String nameOfBestCustomer() {
+		ArrayList<String> names = new ArrayList<String>();
+
+		// variable pour avoir le plus gros amount des clients
+		double amountMax = 0;
+
+		String nameMax = "";
+
+		for (Customer e : list) {
+			if (e.amount > amountMax) {
+				amountMax = e.amount;
+				nameMax = e.name;
+			}
+		}
+		return nameMax;
+	}
+
+	public static void main(String[] args) {
+
+		Store store = new Store();
+
+		store.addSale("Bob", 5);
+		store.addSale("Jane", 69);
+		store.addSale("Kaaris", 12);
+		store.addSale("Lyonne", 120);
+		store.addSale("Booba", 666);
+
+		System.out.println(store.nameOfBestCustomer());
+
+	}// main
+
+}
